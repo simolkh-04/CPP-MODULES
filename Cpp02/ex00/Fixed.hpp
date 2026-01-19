1884a2a510df62948d55af58528ff9928d33833a
lakhal simohammed <simolkh@DESKTOP-756FSBN.localdomain>
2026-01-19 18:22:15 +0100

    commencer cpp02

---
+++ Cpp02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+
+class Fixed {
+    private:
+        int value;
+        static int const bit = 8;
+    public:
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed &operator=(const Fixed &other);
+        ~Fixed();
+        int getRawBits() const;
+        void setRawBits(int const raw);
+};
+
+#endif
