bf5d9afd2c0b7bdd5060372a807d9f08f24ead9f
Mohammed Lakhal <mlakhal@c3r7p14.1337.ma>
2026-01-22 17:12:11 +0100

    ex00 in cpp04

---
+++ Cpp04/ex00/Cat.cpp
@@ -0,0 +1,16 @@
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+    this->type = "Cat";
+    std::cout << "Cat constructor called" << std::endl;
+}
+Cat::~Cat()
+{
+    std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound () const
+{
+    std::cout << "Miow! Miow!" << std::endl;
+}
---
+++ Cpp04/ex00/Dog.cpp
@@ -0,0 +1,16 @@
+#include "Dog.hpp"
+
+Dog::Dog()
+{
+    this->type = "Dog";
+    std::cout << "Dog constructor called" << std::endl;
+}
+Dog::~Dog()
+{
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound () const
+{
+    std::cout << "Woof! Woof!" << std::endl;
+}
