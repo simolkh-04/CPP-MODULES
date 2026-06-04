a546710ae96dfa5a665d376966d94042e85370c2
Mohammed Lakhal <mlakhal@c2r6p12.1337.ma>
2026-06-04 10:57:20 +0100

    r

--- Cpp05/ex02/ShrubberyCreationForm.cpp
+++ Cpp05/ex02/ShrubberyCreationForm.cpp
@@ -2,21 +2,21 @@
 
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target):
 AForm("Shrubbery Creation", 145, 137), _target(target){}
-// ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other):
-// _target(other._target){}
-// ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
-// {
-//     if(this != &other)
-//     {
-//         AForm::operator=(other);
-//         this->_target = other._target;
-//     }
-// }
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other):
+AForm(other), _target(other._target){}
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
+{
+    if(this != &other)
+    {
+        AForm::operator=(other);
+    }
+    return *this;
+}
 ShrubberyCreationForm::~ShrubberyCreationForm(){}
 void ShrubberyCreationForm::execute(Bureaucrat const &executor) const
 {
     beExecuted(executor);
-    std::string filename = _target + "_shrybbery";
+    std::string filename = _target + "_shrubbery";
     std::ofstream outfile(filename.c_str());
     if(!outfile.is_open())
     {
